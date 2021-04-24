0a406e5898be2c9ed31115a688898fd7ae9fc7b0
Melvin Carvalho <melvincarvalho@gmail.com>
2021-04-24 21:02:06 +0200

    DEFAULT_MESSAGE

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,16 +1,38 @@
 #!/bin/bash
 
+TMPFILE="/tmp/gitmark.$$.txt"
 DEFAULT_MESSAGE="webledger"
 MESSAGE="${1:-DEFAULT_MESSAGE}"
+# set up btm as remote exe
+BTMEXE="btm"
+
 
 git pull origin gh-pages
 git add .
 git commit -m "$MESSAGE"
+
+git log -1 --pretty=%s | grep '^gitmark '
+if [ $? -eq 0 ]
+then
+  echo git marked already
+  exit
+fi
+
 git push origin gh-pages
 
 # run twice in case new tx is not there
 git mark
-git mark
+git mark > "${TMPFILE}"
+
+cat "${TMPFILE}"
+
+TX=$( cat "${TMPFILE}" | tail -1 )
+
+echo
+echo "${TX}"
+
+HASH=$(${BTMEXE} ${TX})
+echo "git commit --allow-empty -m ${HASH}"
 
 # TODO
 # run tx
