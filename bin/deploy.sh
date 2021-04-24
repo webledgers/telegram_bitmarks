317f7d960e3e6e72fb6d7f72c0314b42cf42de4f
Melvin Carvalho <melvincarvalho@gmail.com>
2021-04-24 19:18:00 +0200

    DEFAULT_MESSAGE

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -3,9 +3,16 @@
 DEFAULT_MESSAGE="webledger"
 MESSAGE="${1:-DEFAULT_MESSAGE}"
 
+git pull origin gh-pages
 git add .
 git commit -m "$MESSAGE"
 git push origin gh-pages
 
+# run twice in case new tx is not there
+git mark
 git mark
 
+# TODO
+# run tx
+# add commit
+# push
